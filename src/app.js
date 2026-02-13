9666930008c7b0ee03e4ff8d0357cfee91fb9fe9
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2026-02-13 22:25:37 +0530

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app=express();
+require('dotenv').config();
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT,(req,res)=>{
+    console.log(`Server listening at port ${PORT}`);
+})
